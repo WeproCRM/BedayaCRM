11d1ec1fd83f7efd9b70a462bb44b8f021e54035
WeproCRM <bahaay3k@gmail.com>
2026-07-27 23:32:02 +0300

    Add files via upload

---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2870
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 export const SUPER_ADMINS = ['wepro.gr@gmail.com'] as const;\
+\
+export const COLLECTIONS = \{\
+  USERS: 'users',\
+  CLIENTS: 'clients',\
+  TASKS: 'tasks',\
+  NOTIFICATIONS: 'notifications',\
+  CHATS: 'chats',\
+  SETTINGS: 'settings',\
+\} as const;\
+\
+export const DEFAULT_EXCHANGE_RATES = \{\
+  usd: 1,\
+  ils: 3,\
+  sar: 3.75,\
+\};}
