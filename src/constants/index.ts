063e06de8b1c7d32d8f5182e926946678514ef85
WeproCRM <bahaay3k@gmail.com>
2026-07-28 18:31:01 +0300

    Add files via upload

---
+++ src/constants/index.ts
@@ -0,0 +1,16 @@
+export const SUPER_ADMINS = ['wepro.gr@gmail.com'] as const;
+
+export const COLLECTIONS = {
+  USERS: 'users',
+  CLIENTS: 'clients',
+  TASKS: 'tasks',
+  NOTIFICATIONS: 'notifications',
+  CHATS: 'chats',
+  SETTINGS: 'settings',
+} as const;
+
+export const DEFAULT_EXCHANGE_RATES = {
+  usd: 1,
+  ils: 3,
+  sar: 3.75,
+};
